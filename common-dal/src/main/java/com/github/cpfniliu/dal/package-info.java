1bc82694d6b9408407922d9f0f53fb7b3cb20ded
CPF <2484661388@qq.com>
2021-10-06 22:05:27 +0800

    BBEN 最终提交

--- common-dal/src/main/java/com/github/cpfniliu/dal/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * <b>Description : </b>
- *
- * @author CPF
- * Date: 2020/7/1 17:13
- */
-package com.github.cpfniliu.dal;
--- common-dal/src/main/java/com/github/cpfniliu/dal/sql/ele/OrderDirection.java
+++
@@ -1,6 +0,0 @@
-package com.github.cpfniliu.dal.sql.ele;
-
-public enum OrderDirection
-{
-    ASC, DESC
-}
